c99e1c15a609e9c7bb425a6b4d0ea982deca83af
gestra <6391580+gestra@users.noreply.github.com>
2021-11-03 14:12:40 +0200

    HTTP client: Change user agent string to T-botti

--- src/http_client.rs
+++ src/http_client.rs
@@ -6,7 +6,7 @@ use std::time::Duration;
 
 lazy_static! {
     pub static ref HTTP_CLIENT: reqwest::Client = reqwest::Client::builder()
-        .user_agent("Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
+        .user_agent(format!("T-botti/{}", env!("CARGO_PKG_VERSION")))
         .timeout(Duration::from_secs(10))
         .build()
         .unwrap();
